13b1cf279ef957618ebb409e47535675325f6644
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-05-12 20:59:33 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+
+        }
+    }
+}
