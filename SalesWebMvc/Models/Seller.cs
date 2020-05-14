6811859274b386e1075701cfb80a3391dc2d3b33
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-05-14 19:09:45 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -7,19 +7,42 @@ namespace SalesWebMvc.Models
 {
     public class Seller
     {
+        //ID
         public int Id { get; set; }
+
+        //NAME
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage ="Name size should be between 3 and 60")]
+  
         public string Name { get; set; }
+        
+        //EMAIL
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="Enter a valid e-mail")]
+        [Required(ErrorMessage = "{0} required")]
         public string Email { get; set; }
+        
+        //BIRTHDATE
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        //BASESALARY
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.00, 50000.00, ErrorMessage ="{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
+        //DEPARTMENT
         public Department Department { get; set; }
+
+        //DEPARTMENTID
         public int DepartmentId { get; set; }
+
+        //SALES
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
